e2be85904c0327f0cdd4d7c65c79366c68e59970
m0ksuta <olhovskii.m@mail.ru>
2021-01-22 16:07:30 +0300

    Add Postgres

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'myawesomeblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myawesomblogdb',
+        'USER': 'postgres',
+        'PASSWORD': 'turbo2`m',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
